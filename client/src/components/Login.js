455e472acc97d1d2f08413b0bd84bfa03e3057e5
ebisLab <ebaiden@techie.com>
2019-08-23 11:25:26 -0400

    form created

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,13 +1,36 @@
-import React from "react";
+import React, {useState} from "react";
 
 const Login = () => {
   // make a post request to retrieve a token from the api
+  const [grabData, setGrabData] = useState({ username: 'Lambda School', password: 'i<3Lambd4'});
+  const handleChange = event => setGrabData({...grabData, [event.target.name]: event.target.value})
+  const handleSubmit = event => {
+    console.log('handleSubmit button clicked')
+      event.preventDefault();
+      // submitFriend(friend);
+  }
+
+
+
+
   // when you have handled the token, navigate to the BubblePage route
   return (
-    <>
-      <h1>Welcome to the Bubble App!</h1>
-      <p>Build a login page here</p>
-    </>
+    <form onSubmit ={handleSubmit}>
+            <input 
+            type ="text" 
+            name="username"
+            placeholder="username" 
+            value={grabData.username} 
+            onChange={handleChange} />
+
+<input 
+            type ="password" 
+            name="password"
+            placeholder="password" 
+            value={grabData.password} 
+            onChange={handleChange} />
+            <button type="submit">Submit</button>
+        </form>
   );
 };
 
